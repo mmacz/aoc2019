733ac7a32d6c0bcd38ec05e0aba8025e99296954
Marek Macznik <marek.macznik@gmail.com>
2024-06-06 01:03:51 +0200

    Solved day3

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,7 @@
 use std::io::{self};
 use std::fs::File;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 struct Coords {
     dir: i32,
@@ -13,6 +14,8 @@ struct Line {
     tuple: (i32, i32)
 }
 
+
+#[derive(Clone)]
 struct Point {
     x: i32,
     y: i32
@@ -98,6 +101,70 @@ fn manhattan_distance(pt: &Point) -> i32 {
     pt.x.abs() + pt.y.abs()
 }
 
+fn get_min_distance(intersections: &Vec<Point>) -> i32 {
+    let mut min_distance = i32::MAX;
+    for ip in intersections.into_iter() {
+        let distance: i32 = manhattan_distance(&ip);
+        if min_distance > distance {
+            min_distance = distance;
+        }
+    }
+    min_distance
+}
+
+fn is_intersection(x: i32, y: i32, intersections: &Vec<Point>) -> bool {
+    for ip in intersections {
+        /* Inverted entries in wrapper */
+        if y == ip.x && x == ip.y {
+            return true;
+        }
+    }
+    false
+}
+
+fn build_key(x: i32, y: i32) -> u64 {
+    (x as u64) << 32 | y as u64
+}
+
+fn get_min_steps(wires: &Vec<Vec<Coords>>, intersections: &Vec<Point>) -> u64 {
+    let mut steps: HashMap<u64, u32> = HashMap::new();
+    let mut key: u64 = 0;
+    for wire in wires {
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+        let mut step: u32 = 0;
+        for c in wire {
+            for _s in 0..c.steps {
+                if c.is_vert {
+                    y += c.dir;
+                }
+                else {
+                    x += c.dir;
+                }
+                step += 1;
+                if is_intersection(x, y, &intersections) {
+                    key = build_key(x, y);
+                    if steps.contains_key(&key) {
+                        let new_step = steps.get(&key).unwrap() + step;
+                        steps.insert(key, new_step);
+                    }
+                    else {
+                        steps.insert(key, step);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut min_steps: u64 = u64::MAX;
+    for s in steps {
+        if min_steps > s.1 as u64 {
+            min_steps = s.1 as u64;
+        }
+    }
+    min_steps
+}
+
 pub fn solution(input: io::Lines<io::BufReader<File>>) -> () {
     let mut wires: Vec<Vec<Coords>> = Vec::new();
     for line in input.flatten() {
@@ -113,13 +180,7 @@ pub fn solution(input: io::Lines<io::BufReader<File>>) -> () {
     let mut intersections = get_intersections(&lines1.0, &lines2.1);
     intersections.extend(get_intersections(&lines2.0, &lines1.1));
 
-    let mut min_distance = i32::MAX;
-    for ip in intersections {
-        let distance: i32 = manhattan_distance(&ip);
-        if min_distance > distance {
-            min_distance = distance;
-        }
-    }
-    println!("Answer 1: {}", min_distance);
+    println!("Answer 1: {}", get_min_distance(&intersections));
+    println!("Answer 2: {}", get_min_steps(&wires, &intersections));
 }
 
