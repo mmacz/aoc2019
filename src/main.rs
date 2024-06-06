b6767254d391577c46365499f4956707be740401
Marek Macznik <marek.macznik@gmail.com>
2024-06-06 22:32:52 +0200

    Add: Solution for th 4th day

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::env;
-pub mod day3;
+pub mod day4;
 pub mod utils;
 
 fn main() {
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("File with input is not provided");
-    }
-    if let Ok(lines) = utils::read_lines(&args[1]) {
-        day3::solution(lines);
-    }
-    else {
-        panic!("Cannot read the file");
-    }
+    day4::solution();
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() != 2 {
+    //     panic!("File with input is not provided");
+    // }
+    // if let Ok(lines) = utils::read_lines(&args[1]) {
+        
+    // }
+    // else {
+    //     panic!("Cannot read the file");
+    // }
 }
 
