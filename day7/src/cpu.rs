f68d7b7c964f4e8f4aabb6fb20857c1760b9173a
Marek Macznik <marek.macznik@gmail.com>
2024-07-24 23:01:27 +0200

    Rework CPU into struct & impl

---
+++ day7/src/cpu.rs
@@ -0,0 +1,127 @@
+
+pub struct Cpu {
+    pub out: i32,
+    
+    code: Vec<i32>,
+    phase: i32,
+    pc: usize,
+    phase_registered: bool,
+}
+
+impl Cpu {
+    pub fn new(phase: i32, code: &Vec<i32>) -> Cpu {
+        return Cpu{
+            out: 0,
+            code: code.to_vec(),
+            phase: phase,
+            pc: 0,
+            phase_registered: false,
+        };
+    }
+
+    pub fn process(&mut self, input: &i32) -> bool {
+        let mut done: bool = false;
+
+        if self.pc >= self.code.len() {
+            return true; // done
+        }
+
+        let mut code: i32 = self.code[self.pc];
+        let opcode: i32 = self.parse_code(&mut code, 100);
+        let op1_mode: i32 = self.parse_code(&mut code, 10);
+        let op2_mode: i32 = self.parse_code(&mut code, 10);
+        let _op3_mode: i32 = self.parse_code(&mut code, 10);
+
+        match opcode {
+            1 => { // add
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                let op3: i32 = self.get_operand_by_mode(1, 3);
+                self.code[op3 as usize] = op1 + op2;
+                self.pc += 4;
+            },
+            2 => { // mul
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                let op3: i32 = self.get_operand_by_mode(1, 3);
+                self.code[op3 as usize] = op1 * op2;
+                self.pc += 4;
+            },
+            3 => { // ld
+                let param: i32;
+                match self.phase_registered {
+                    false => { param = self.phase; self.phase_registered = true; },
+                    true => param = *input,
+                }
+                if param == -1 {
+                    return false;
+                }
+                let op1: usize = self.get_operand_by_mode(1, 1) as usize;
+                self.code[op1] = param;
+                self.pc += 2;
+            },
+            4 => { // rd
+                let op1: usize = self.get_operand_by_mode(1, 1) as usize;
+                self.out = self.code[op1];
+                self.pc += 2;
+            },
+            5 => { //jt
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                match op1 {
+                    0 => self.pc += 3,
+                    _ => self.pc = op2 as usize
+                }
+            },
+            6 => { // jf
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                match op1 {
+                    0 => self.pc = op2 as usize,
+                    _ => self.pc += 3,
+                }
+            },
+            7 => { // lt
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                let op3: i32 = self.get_operand_by_mode(1, 3);
+                self.code[op3 as usize] = if op1 < op2 { 1 } else { 0 };
+                self.pc += 4;
+            },
+            8 => { // eq
+                let op1: i32 = self.get_operand_by_mode(op1_mode, 1);
+                let op2: i32 = self.get_operand_by_mode(op2_mode, 2);
+                let op3: i32 = self.get_operand_by_mode(1, 3);
+                self.code[op3 as usize] = if op1 == op2 { 1 } else { 0 };
+                self.pc += 4;
+            },
+            99 => { // stop
+                done = true
+            }
+            _ => panic!("Unknown opcode: {}", opcode),
+        }
+
+        done // not done
+    }
+
+    fn parse_code(&mut self, code: &mut i32, digits: i32) -> i32 {
+        let rem: i32 = *code % digits;
+        *code = *code / digits;
+        rem
+    }
+
+    fn get_operand_by_mode(&mut self, mode: i32, op_idx: usize) -> i32 {
+        let mut op: i32;
+        match mode {
+            0 => {
+                op = self.code[self.pc + op_idx];
+                op = self.code[op as usize];
+            },
+            1 => {
+                op = self.code[self.pc + op_idx];
+            }
+            _ => panic!("Unknown operand mode: {}", mode),
+        }
+        op
+    }
+}
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,11 +2,10 @@ use std::env;
 use std::fs;
 use std::string::String;
 use std::str::FromStr;
-use std::collections::VecDeque;
 use itertools::Itertools;
 
-pub mod computer;
 pub mod sanity_inputs;
+pub mod cpu;
 
 fn get_intcode_from_str(str_code: &str) -> Vec<i32> {
     str_code.split(",").filter_map(|c| i32::from_str(c).ok()).collect()
@@ -19,9 +18,13 @@ fn solution1(file: &String) -> i32 {
     for phases in permutations {
         let mut out: i32 = 0;
         for phase in phases {
-            let mut inputs: VecDeque<i32> = vec!{phase, out}.into();
-            let mut code: Vec<i32> = get_intcode_from_str(&file);
-            out = computer::intcode_decode(&mut code, &mut inputs);
+            let code: Vec<i32> = get_intcode_from_str(&file);
+            let mut amp: cpu::Cpu = cpu::Cpu::new(phase, &code);
+            let mut done: bool = false;
+            while !done {
+                done = amp.process(&out);
+            }
+            out = amp.out;
         }
         if out > max_out {
             max_out = out;
@@ -50,9 +53,9 @@ fn main() {
     if args.len() != 2 {
         panic!("File with input is not provided");
     }
-    let file: String = fs::read_to_string(&args[1]).unwrap();
-
-    println!("Answer 1: {}", solution1(&file));
+    let input: String = fs::read_to_string(&args[1]).unwrap();
+    println!("Answer 1: {}", solution1(&input));
+    assert_eq!(255840, solution1(&input));
     println!("Answer 2: {}", solution2(&sanity_inputs::sanity4()));
 }
 
