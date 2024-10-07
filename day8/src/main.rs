7ba8c10cc651c5825a1e77fc641261af348d09f6
Marek Macznik <marek.macznik@gmail.com>
2024-10-08 00:42:01 +0200

    Day 8 completed

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -34,7 +34,41 @@ fn solution1(input: &String, width: usize, height: usize) -> usize {
     ones_cnt * twos_cnt
 }
 
+fn print_img(img: &Vec<u32>, width: usize) {
+    println!("Answer 2:");
+    for chunk in img.chunks_exact(width) {
+        println!("{:?}", chunk);
+    }
+}
+
+fn solution2(input: &String, width: usize, height: usize) -> Vec<u32> {
+    let layers = get_layers(&input, &width, &height);
+    let first_layer = layers[0].clone();
+
+    let result = first_layer.into_iter()
+        .enumerate()
+        .map ( |(idx, pixel)| {
+            layers.iter()
+                .skip(1) // 1st layer is already present
+                .fold(pixel, |acc, layer| {
+                    let r: u32;
+                    match acc {
+                        0 => { r = 0},
+                        1 => { r = 1},
+                        2 => { r = layer[idx]},
+                        _ => panic!("Invalid pixel value")
+                    }
+                    r
+                })
+        })
+        .collect();
+    result
+}
+
+const IM_WIDTH: usize = 25;
+const IM_HEIGHT: usize = 6;
 fn main() {
-    println!("Answer 1: {}", solution1(&input::puzzle_input(), 25, 6));
+    println!("Answer 1: {}", solution1(&input::puzzle_input(), IM_WIDTH, IM_HEIGHT));
+    print_img(&solution2(&input::puzzle_input(), IM_WIDTH, IM_HEIGHT), IM_WIDTH);
 }
 
--- day8/src/sanity_inputs.rs
+++ day8/src/sanity_inputs.rs
@@ -4,3 +4,7 @@ pub fn sanity1() -> String {
     "123456789012".to_string()
 }
 
+pub fn sanity2() -> String {
+    "0222112222120000".to_string()
+}
+
